7e3071a20e37a2845df33dae8e53587744e77b3c
mathias1902 <gmathias1717@gmail.com>
2021-08-21 19:41:48 -0300

    Enviando o formulário de contato via POST

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 // Agrupamento --> /app
