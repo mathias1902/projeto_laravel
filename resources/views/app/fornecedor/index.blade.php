fad32f835a18fca33e765e66db3d76b0f05c87ba
Gabriel Mathias <gmathias1717@gmail.com>
2021-08-21 16:50:05 -0300

    Blade - @while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -15,7 +15,8 @@
 @endphp
 
 @isset($fornecedores)
-    @for($i = 0; isset($fornecedores[$i]); $i++)
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
         Fornecedor: {{ $fornecedores[$i]['nome'] }}
         <br />
         Status: {{ $fornecedores[$i]['status'] }}
@@ -24,5 +25,6 @@
         <br />
         Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
         <hr />
-    @endfor
+        @php $i++ @endphp
+    @endwhile
 @endisset
