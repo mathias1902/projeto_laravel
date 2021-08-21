c56a2eefe1e9e5cd8e78f2d3c8996734fc7b78cf
Gabriel Mathias <gmathias1717@gmail.com>
2021-08-21 10:00:26 -0300

    Rotas - Parâmetros opcionais
    Definição de rotas com parâmetros opcionais.

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
+// nome, categoria, assunto, mensagem
+
+Route::get(
+    '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+    function(
+            string $nome = 'Teste',
+            string $categoria = 'Informação',
+            string $assunto = 'Contato',
+            string $mensagem = 'mensagem não informada') {
+        /* 
+            Escrito com aspas simples, precisa concatenar.
+            echo 'Estamos aqui: '.$nome.' - '.$categoria.' - '.$assunto.' - '.$mensagem; 
+        */
+
+        // Exemplo com aspas duplas.
+        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+    }
+);
 
 /* Verbos para controlar as requisições feitas para o servidor:
 
