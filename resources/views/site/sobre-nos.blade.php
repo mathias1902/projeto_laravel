93071bf88471ec74069b7f2418d53b0df6a4393a
Gabriel Mathias <gmathias1717@gmail.com>
2021-08-21 10:42:04 -0300

    Nomeando Rotas
    Técnica para evitar dependência da rota em si, onde se alterar a rota não haverá problemas nas demais views.

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -3,12 +3,12 @@
 <!-- Links -->
 <ul>
     <li>
-        <a href="/">Principal</a>
+        <a href="{{ route('site.index') }}">Principal</a>
     </li>
     <li>
-        <a href="/sobre-nos">Sobre Nós</a>
+    <a href="{{ route('site.sobrenos') }}">Sobre Nós</a>
     </li>
     <li>
-        <a href="/contato">Contato</a>
+    <a href="{{ route('site.contato') }}">Contato</a>
     </li>
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,14 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
-Route::get('/login', function(){ return 'Login'; });
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 // Agrupamento --> /app
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function(){ return 'Clientes'; });
-    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
-    Route::get('/produtos', function(){ return 'Produtos'; });
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('site.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('site.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('site.produtos');
 });
