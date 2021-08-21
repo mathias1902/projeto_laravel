0451be15331511fb9fdeb6036f2261934dce5457
Gabriel Mathias <gmathias1717@gmail.com>
2021-08-21 16:28:53 -0300

    Blade - Operador condicial de valor default

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -11,20 +11,14 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00'
+                'cnpj' => '0'
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
-                'status' => 'S'
+                'status' => 'S',
+                'cnpj' => null
             ]
         ];
-
-        /*
-            condicao ? se verdadeiro : se falso; // operador ternário
-            condicao ? se verdadeiro : (condicao ? se verdadeiro : se falso); // ternário com encadeamento
-        */
-        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
-        echo $msg;
 
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -19,10 +19,10 @@
     <br />
     Status: {{ $fornecedores[0]['status'] }}
     <br />
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{ $fornecedores[0]['cnpj'] }}
-        @empty($fornecedores[0]['cnpj'])
-            - Vazio
-        @endempty    
-    @endisset
+    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido' }}
+    <!--
+        $variavel teste não estiver definida (isset)
+        ou
+        $variavel testada possuir o valor null
+    -->
 @endisset
