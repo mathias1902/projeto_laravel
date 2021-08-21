3d620e093512ce4609f9a44fd30e23fec1e68566
Gabriel Mathias <gmathias1717@gmail.com>
2021-08-21 15:56:07 -0300

    Blade - @unless
    Funciona de forma semelhante ao if, porém executa quando o resultado da operação for false.

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,17 +8,7 @@ class FornecedorController extends Controller
 {
     public function index() {
         $fornecedores = [
-            'Fornecedor 1',
-            'Fornecedor 2',
-            'Fornecedor 3',
-            'Fornecedor 4',
-            'Fornecedor 5',
-            'Fornecedor 6',
-            'Fornecedor 7',
-            'Fornecedor 8',
-            'Fornecedor 9',
-            'Fornecedor 10',
-            'Fornecedor 11'
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'N']
         ];
 
         return view('app.fornecedor.index', compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,20 +2,23 @@
 
 @php
     /*
-    if() {
-
-    } elseif() {
-
-    } else {
-
-    }
+    if(!<condicao>) { } //enquanto o if executa se o retorno for true
     */
 @endphp
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
+{{-- @unless executa se o retorno for false --}}
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br />
+Status: {{ $fornecedores[0]['status'] }}
+<br />
+
+@if( !($fornecedores[0]['status'] == 'S') )
+    Fornecedor inativo
 @endif
+
+<br />
+
+@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
+    Fornecedor inativo
+@endunless
