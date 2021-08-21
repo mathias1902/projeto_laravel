c0f6b182c6d0e4f773cfd9164c620c10d518dd94
Gabriel Mathias <gmathias1717@gmail.com>
2021-08-21 16:07:32 -0300

    Blade - @isset
    Aqui podemos verificar a existência da variável.

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,23 +2,16 @@
 
 @php
     /*
-    if(!<condicao>) { } //enquanto o if executa se o retorno for true
+    if(isset($variavel)) { } //retornar true se a variável estiver definida
     */
 @endphp
 
-{{-- @unless executa se o retorno for false --}}
-
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br />
-Status: {{ $fornecedores[0]['status'] }}
-<br />
-
-@if( !($fornecedores[0]['status'] == 'S') )
-    Fornecedor inativo
-@endif
-
-<br />
-
-@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
-    Fornecedor inativo
-@endunless
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br />
+    Status: {{ $fornecedores[0]['status'] }}
+    <br />
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+    @endisset
+@endisset
