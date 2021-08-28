ab2ce9e0fe03182f404e8505825be99cb74c7d6d
mathias1902 <gmathias1717@gmail.com>
2021-08-28 13:13:33 -0300

    Customizando a apresentação dos erros de validação - parte 1

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -19,8 +19,11 @@
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-<div style="position:absolute; right:0px; top:0px;  width:100%; background:red;">
-    <pre>
-        {{print_r($errors)}}
-    </pre>
-</div
+@if ($errors->any())
+    <div style="position:absolute; right:0px; top:0px;  width:100%; background:red;">
+        @foreach ( $errors->all() as $erro)
+            {{ $erro }}
+            <br />
+        @endforeach
+    </div>
+@endif
