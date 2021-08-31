0301f8b1b8b3286fb7db0d5d47d60ea5ab0e3768
Gabriel Mathias <gmathias1717@gmail.com>
2021-08-31 09:44:42 -0300

    Corrigido erro na rota "/fornecedor"

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     Route::get('/home', 'HomeController@index' )->name('app.home');
     Route::get('/sair', 'LoginController@sair' )->name('app.sair');
     Route::get('/cliente', 'ClienteController@index' )->name('app.cliente');
-    Route::get('/fornecedore', 'FornecedorController@index')->name('app.fornecedor');
+    Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
     Route::get('/produto', 'ProdutoController@index' )->name('app.produto');
 });
 
